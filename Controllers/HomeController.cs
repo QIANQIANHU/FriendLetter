5536a53b254920803bec52fe7344fd2a3e83cdfe
Faiza Husain and Qianqian Hu <faiza@>
2018-02-05 16:22:24 -0800

    add forms

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,8 +29,25 @@ namespace FriendLetter.Controllers
       public ActionResult Hello()
        {
          LetterVariable myLetterVariable = new LetterVariable();
-         myLetterVariable.SetRecipient("Jessica");
+         myLetterVariable.SetRecipient("Qianqian");
+         myLetterVariable.SetSender("Faiza");
          return View(myLetterVariable);
       }
+
+      [Route("/form")]
+      public ActionResult Form()
+      {
+        return View();
+      }
+
+      [Route("/greeting_card")]
+        public ActionResult GreetingCard()
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.SetRecipient(Request.Query["recipient"]);
+            myLetterVariable.SetSender(Request.Query["sender"]);
+            return View("Hello", myLetterVariable);
+        }
+
     }
 }
